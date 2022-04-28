5bba7f60e4b063b649342d533f4554c149192493
NOOBiie-007 <77447769+NOOBiie-007@users.noreply.github.com>
2022-04-28 23:11:18 +0600

    For and While And Switch

---
+++ Loop.js
@@ -0,0 +1,29 @@
+var num =10;
+while( num < 15){
+     console.log(num);
+     num++;
+}
+
+
+var num =[12,45,67,45,24,77];
+for( var i=0; i<num.length; i++){
+     console.log(num[i]);
+}
+
+
+var num = 100;
+switch(num){
+     case 1000:
+          console.log('Here I am 1000');
+          break;
+     case 500:
+          console.log('Here I am 500');
+          break;
+     case 100:
+          console.log('Here I am 100');
+          break;
+     default:
+          console.log('Here I am nothing');
+
+
+}
